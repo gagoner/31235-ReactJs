a7aa2088eea5ac2e9db377a81fc02f6fe9900526
Gabriel Gonzalez <gagoner@gmail.com>
2022-07-05 00:09:34 -0400

    Desafío 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,22 @@
 import logo from './logo.png';
 
-const NavBar = {
-    return <nav>
-    <div>
-        <a href="index.html" class="nav__logo--izq"><img src={logo} alt="Logo gagoner, un apret&oacute;n de manos" height="13"></a>
-    </div>
-    <div>
-        <ul>
-            <li><a href="index.html">Inicio</a></li>
-            <li><a href="#">Sobre m&iacute;</a></li>
-            <li><a href="#">Servicios</a></li>
-            <li><a href="#">Experiencia</a></li>
-            <li><a href="#">Contacto</a></li>
-        </ul>
-    </div>
-</nav>;
+const NavBar = () => {
+    return (
+    <nav>
+        <div>
+            <a href="index.html" class="nav__logo--izq"><img src={logo} alt="Logo gagoner, un apret&oacute;n de manos" height="13"/></a>
+        </div>
+        <div>
+            <ul>
+                <li><a href="index.html">Inicio</a></li>
+                <li><a href="#">Sobre m&iacute;</a></li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="#">Experiencia</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </div>
+    </nav>
+    );
 };
 
 export default NavBar;
