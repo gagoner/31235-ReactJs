fefecaf8e4fee7f9a72e2f9e8f8d82f7e564ad1b
Gabriel Gonzalez <gagoner@gmail.com>
2022-07-05 00:22:03 -0400

    Desafío 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,20 +2,33 @@ import logo from './logo.png';
 
 const NavBar = () => {
     return (
-    <nav>
-        <div>
-            <a href="index.html" class="nav__logo--izq"><img src={logo} alt="Logo gagoner, un apret&oacute;n de manos" height="13"/></a>
-        </div>
-        <div>
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="#">Sobre m&iacute;</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="#">Experiencia</a></li>
-                <li><a href="#">Contacto</a></li>
-            </ul>
-        </div>
-    </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+		<div className="container-fluid">
+			<a href="index.html" className="navbar-brand"><img src={logo} alt="Logo gagoner, un apret&oacute;n de manos" height="13" /></a>
+			<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+					<li className="nav-item">
+						<a className="nav-link active nav__a--active" aria-current="page" href="index.html">Inicio</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Sobre m&iacute;</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Servicios</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Experiencia</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Contacto</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
     );
 };
 
