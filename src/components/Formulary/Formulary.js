4022f5dae1413d9958dd5901e7b059158489c1e7
gagoner <gagoner@gmail.com>
2022-08-31 04:50:52 -0400

    Corrección a la validación del formulario

--- src/components/Formulary/Formulary.js
+++ src/components/Formulary/Formulary.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 import { useCartContext } from "../../context/cartContext";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Formulary() {
 
     const { clearCart, totalPrice } = useCartContext();
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
 
     const [form, setForm] = useState({
         name: "",
@@ -21,54 +26,56 @@ function Formulary() {
         email: "",
         address: "",
     });
-    
+
+
     //handle submit updates
     function handleChange(event) {
         const { name, value } = event.target;
         setForm({ ...form, [name]: value });
     }
-    
-    const checkValidation = () => {
+
+    useEffect(() => {
+
         let errors = validation;
     
         //first Name validation
         if (!form.name.trim()) {
             errors.name = "Por favor, ingrese su nombre";
-        } else {
-            errors.name = "";
-        }
+        } else if (form.name.length < 4) {
+            errors.name = "El nombre no es válido";
+        } else {errors.name = ""}
+
         //Phone validation
-        if (!form.phone.trim()) {
+        form.phone = form.phone.replace(/^\+/g, 0)
+        if (form.phone.trim().length===0) {
             errors.phone = "Por favor, ingrese su teléfono";
-        } else if (form.phone.length < 10 || form.phone.length > 17) {
-            errors.phone = "El teléfono no es válido";
+        } else if (form.phone.replace(/\D/g, "").length > 7 && form.phone.replace(/\D/g, "").length < 12) {
+            errors.phone = ""
+        } else {
+            errors.phone = "El teléfono no es válido"
         }
     
         // email validation
-        const emailCond =
-        "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/";
-        if (!form.email.trim()) {
+        if (form.email.trim().length===0) {
             errors.email = "Se requiere ingresar un email";
-        } else if (!form.email.match(emailCond)) {
-            errors.email = "Por favor, ingrese su email";
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(form.email)) {
+            errors.email = "El email no es válido";
         } else {
             errors.email = "";
         }
     
         //address validation
-        if (!form.address.trim()) {
+        if (form.address.trim().length===0) {
             errors.address = "La dirección es requerida";
-        }
+        } else {errors.address = ""}
     
         setValidation(errors);
-    };
 
-    useEffect(() => {
-        checkValidation();
-    }, [form]);
+    }, [form, validation]);
     
     const handleSubmit = (e) => {
         e.preventDefault();
+        setShow(true);
     };
 
     return (
@@ -76,43 +83,55 @@ function Formulary() {
         <div className="d-flex justify-content-center">
             <Form className="m-5 needs-validation" onSubmit={handleSubmit}>
                 <h3>
-                    Ingresa tus datos para proceder con la compra N° {localStorage.getItem("orderId")}.
+                    Ingresa tus datos para proceder con la compra.
                 </h3>
                 <Form.Label htmlFor="name">
                     Ingrese su nombre y apellido
                 </Form.Label>
-                <Form.Control type="text" name="name" placeholder="Gonzalo González" required value={form.name} onChange={(e) => handleChange(e)}/>
-                {form.name && <p>{form.name}</p>}
+                <Form.Control type="text" name="name" placeholder="Gonzalo González" autoComplete="off" required value={form.name} onChange={(e) => handleChange(e)}/>
+                {validation.name && <p className="text-danger">{validation.name}</p>}
                 <Form.Label htmlFor="email" >
                     Email
                 </Form.Label>
-                <Form.Control type="email" name="email" placeholder="usuario@dominio.com" required value={form.email} onChange={(e) => handleChange(e)}/>
-                {form.email && <p>{form.email}</p>}
+                <Form.Control type="email" name="email" placeholder="usuario@dominio.com" autoComplete="off" required value={form.email} onChange={(e) => handleChange(e)} onBlur={(e) => handleChange(e)}/>
+                {validation.email && <p className="text-danger">{validation.email}</p>}
                 <Form.Label htmlFor="phone">
                     Teléfono
                 </Form.Label>
-                <Form.Control type="phone" name="phone" placeholder="+56 9 1 234 56 78" required value={form.phone} onChange={(e) => handleChange(e)}/>
-                {form.phone && <p>{form.phone}</p>}
+                <Form.Control type="phone" name="phone" placeholder="+56 9 1 234 56 78" autoComplete="off" required value={form.phone} onChange={(e) => handleChange(e)}/>
+                {validation.phone && <p className="text-danger">{validation.phone}</p>}
                 <Form.Label htmlFor="address">
                     Dirección
                 </Form.Label>
-                <Form.Control type="text" name="address" placeholder="Calle Número, Comuna, Ciudad, Región" required value={form.address} onChange={(e) => handleChange(e)}/>
-                {form.address && <p>{form.address}</p>}
-                <h4 className="text-center my-2">¡Gracias por su compra!</h4>
+                <Form.Control type="text" name="address" placeholder="Calle Número, Comuna, Ciudad, Región" autoComplete="off" required value={form.address} onChange={(e) => handleChange(e)}/>
+                {validation.address && <p className="text-danger">{validation.address}</p>}
                 <div className="d-flex justify-content-center">
                     <Link to="/cart" >
                         <Button variant="secondary" className="m-2" >
                             Volver al carrito
                         </Button>
                     </Link>
-                    <Link to="/" >
-                        <Button variant="primary" type="submit" onClick={clearCart} className="m-2">
+                        <Button variant="primary" type="submit" onClick={handleSubmit} className="m-2">
                             Enviar orden por {totalPrice()}
                         </Button>
-                    </Link>
                 </div>
             </Form>
         </div>
+        <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} >
+            <Modal.Header closeButton>
+                <Modal.Title>¡Gracias por su compra!</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                Su pedido N° {localStorage.getItem("orderId")} por {totalPrice()} ya fue generado. Revise su correo electrónico para más antecedentes.
+            </Modal.Body>
+            <Modal.Footer>
+                <Link to="/" >
+                    <Button variant="secondary" onClick={() => {clearCart(); handleClose()}}>
+                        Cerrar
+                    </Button>
+                </Link>
+            </Modal.Footer>
+        </Modal>
         </>
     );
 };
